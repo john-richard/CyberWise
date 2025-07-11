29f41e1eae87cef28c0179d0a3a0375f2c1dccf3
John Richard Agad <jagad@quadx.xyz>
2025-07-11 20:41:58 +0800

    Fixing production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
         Paginator::defaultView('components.pagination');
         Blade::component('components.footer', 'footer');
         Blade::component('components.navigation', 'navigation');
