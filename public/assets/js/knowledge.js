03c28b1e23e78ee80ac188c3636fd796f1b1e4bb
John Richard Agad <jagad@quadx.xyz>
2025-07-23 04:52:31 +0800

    Test authentication

--- public/assets/js/knowledge.js
+++ public/assets/js/knowledge.js
@@ -275,7 +275,6 @@ async function handleKnowledgeFormSubmission() {
             if (response?.data?.errorInfo) {
                 alert(`ERROR> ${response.data.errorInfo.ERROR}`);
             } else {
-                alert( response.data.redirect_url);
                 window.location.href = response.data.redirect_url;
             }
 
