d183543c34c4c8e56179821fc91ae41ae49fc486
akosimio <romeo.fajilanjr@gmail.com>
2025-07-28 21:41:54 +0800

    Updated option to display featured thread in admintool - disregard status

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -172,7 +172,7 @@ class DashboardController extends Controller
 
 
         // get learning hub threads
-        $threads = $this->featuredThreadService->getTestYourKnowledgeWithFilters($perPage, $filters, false);
+        $threads = $this->featuredThreadService->getTestYourKnowledgeWithFilters($perPage, $filters);
         
         return view('admin.knowledge', 
          [
@@ -217,7 +217,7 @@ class DashboardController extends Controller
 
 
         // get learning hub threads
-        $threads = $this->featuredThreadService->getSelfAssessmentWithFilters($perPage, $filters, false);
+        $threads = $this->featuredThreadService->getSelfAssessmentWithFilters($perPage, $filters);
         
         return view('admin.self-assessment', 
          [
